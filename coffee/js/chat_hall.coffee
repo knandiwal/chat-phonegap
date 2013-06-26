a0f4667f7cfe75320708db0946d348e1f885bde9
raykin <raykincoldxiao@gmail.com>
2013-06-26 15:37:25 +0800

    implement send msg on client side

--- coffee/js/chat_hall.coffee
+++ coffee/js/chat_hall.coffee
@@ -1,6 +1,31 @@
-chatModule = angular.module("chat", []);
+chatModule = angular.module "chat", []
 
-chatModule.controller 'ChatHallCtrl', ($scope)->
+chatModule.config ($routeProvider)->
+  $routeProvider.when('/chatWith/:name',
+    templateUrl: 'chat_with.html'
+    controller: 'ChatCtrl').
+    when('/',
+      templateUrl: 'chat_hall.html'
+      controller: 'ChatHallCtrl'
+    )
+
+chatModule.service 'chatData', ->
+  currentUser: null
+  totalMsgs: []
+  setCurrentUser: (user) ->
+    this.currentUser = user
+  sendMsg: (msg) ->
+    pair =  _.find this.totalMsgs, (pair)-> pair.partner == msg.receiver
+    if pair
+      pair.groupMsgs.push(msg)
+    else
+      pair = { partner: msg.receiver, groupMsgs: []}
+      pair.groupMsgs.push(msg)
+      this.totalMsgs.push(pair)
+  receiveMsg: (msg) ->
+      # Not implemented
+
+chatModule.controller 'ChatHallCtrl', ($scope, chatData)->
   $scope.connectedChatters = []
   $scope.joined = false
 
@@ -13,3 +38,19 @@ chatModule.controller 'ChatHallCtrl', ($scope)->
     # $scope.connectedChatters.
     # define socket here
     $scope.joined = true
+    chatData.setCurrentUser({name: $scope.username})
+    # $scope.currentUser = {username: $scope.username}
+    # $window.currentUser = $scope.currentUser
+
+chatModule.controller 'ChatCtrl', ($scope, $routeParams, chatData)->
+  $scope.partner = {name: $routeParams.name || 'None'}
+  $scope.currentUser = chatData.currentUser
+
+  $scope.getMsgs = ->
+    $scope.msgs = _.find chatData.totalMsgs, (msg)->
+      msg.partner == $scope.partner
+
+  $scope.send = ->
+    msg = { sender: $scope.currentUser, receiver: $scope.partner, content: $scope.msgContent }
+    chatData.sendMsg(msg)
+    $scope.getMsgs()
--- js/chat_hall.js
+++ js/chat_hall.js
@@ -4,13 +4,75 @@
 
   chatModule = angular.module("chat", []);
 
-  chatModule.controller('ChatHallCtrl', function($scope) {
+  chatModule.config(function($routeProvider) {
+    return $routeProvider.when('/chatWith/:name', {
+      templateUrl: 'chat_with.html',
+      controller: 'ChatCtrl'
+    }).when('/', {
+      templateUrl: 'chat_hall.html',
+      controller: 'ChatHallCtrl'
+    });
+  });
+
+  chatModule.service('chatData', function() {
+    return {
+      currentUser: null,
+      totalMsgs: [],
+      setCurrentUser: function(user) {
+        return this.currentUser = user;
+      },
+      sendMsg: function(msg) {
+        var pair;
+        pair = _.find(this.totalMsgs, function(pair) {
+          return pair.partner === msg.receiver;
+        });
+        if (pair) {
+          return pair.groupMsgs.push(msg);
+        } else {
+          pair = {
+            partner: msg.receiver,
+            groupMsgs: []
+          };
+          pair.groupMsgs.push(msg);
+          return this.totalMsgs.push(pair);
+        }
+      },
+      receiveMsg: function(msg) {}
+    };
+  });
+
+  chatModule.controller('ChatHallCtrl', function($scope, chatData) {
     $scope.connectedChatters = [];
     $scope.joined = false;
     $scope.connectedChatters.push('Tester');
     $scope.connectedChatters.push('Anny');
     return $scope.join = function() {
-      return $scope.joined = true;
+      $scope.joined = true;
+      return chatData.setCurrentUser({
+        name: $scope.username
+      });
+    };
+  });
+
+  chatModule.controller('ChatCtrl', function($scope, $routeParams, chatData) {
+    $scope.partner = {
+      name: $routeParams.name || 'None'
+    };
+    $scope.currentUser = chatData.currentUser;
+    $scope.getMsgs = function() {
+      return $scope.msgs = _.find(chatData.totalMsgs, function(msg) {
+        return msg.partner === $scope.partner;
+      });
+    };
+    return $scope.send = function() {
+      var msg;
+      msg = {
+        sender: $scope.currentUser,
+        receiver: $scope.partner,
+        content: $scope.msgContent
+      };
+      chatData.sendMsg(msg);
+      return $scope.getMsgs();
     };
   });
 
